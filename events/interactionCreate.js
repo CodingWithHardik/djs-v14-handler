a20fb053a770c3917497fa0e53724b5aaf39b975
Hardik Gupta <113587014+CodingWithHardik@users.noreply.github.com>
2023-02-11 16:56:39 +0530

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -7,7 +7,7 @@ client.on('interactionCreate', async (interaction) => {
         if (!cmd) return interaction.followUp({ content: `An error has occured`});
         const args = [];
         for (let option of interaction.options.data) {
-            if (option.type === "SUB_COMMAND") {
+            if (option.type === 1) {
                 if (option.name) args.push(option.name);
                 option.options?.forEach((x) => {
                     if (x.value) args.push(x.value);
